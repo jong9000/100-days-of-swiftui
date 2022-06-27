0b21be80fcc27e5f2c2bc4e9e025ac1ec4b4e9e3
Jon Gentry <jonmarkgentry@gmail.com>
2022-06-27 07:04:22 -0700

    Complet tutorial.

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Jonathan Gentry on 6/21/22.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
